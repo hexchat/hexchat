59bc75fb1c39aa981afe5bf3185c927be0b272b1
zed <zed@893a96be-7f27-4fdf-9d1e-6aeec9d3cce1>
2003-05-11 17:09:33 +0000

    make modal
    git-svn-id: https://xchat.svn.sourceforge.net/svnroot/xchat@323 893a96be-7f27-4fdf-9d1e-6aeec9d3cce1

--- src/fe-gtk/about.c
+++ src/fe-gtk/about.c
@@ -88,6 +88,7 @@ menu_about (GtkWidget * wid, gpointer sess)
 	GtkWidget *vbox, *label, *hbox;
 	char buf[512];
 	const char *locale = NULL;
+	extern GtkWindow *parent_window;      /* maingui.c */
 
 	if (about)
 	{
@@ -99,6 +100,8 @@ menu_about (GtkWidget * wid, gpointer sess)
 	gtk_window_set_position (GTK_WINDOW (about), GTK_WIN_POS_CENTER);
 	gtk_window_set_resizable (GTK_WINDOW (about), FALSE);
 	gtk_window_set_title (GTK_WINDOW (about), _("About X-Chat"));
+	if (parent_window)
+		gtk_window_set_transient_for (GTK_WINDOW (about), parent_window);
 	g_signal_connect (G_OBJECT (about), "destroy",
 							G_CALLBACK (about_close), 0);
 
