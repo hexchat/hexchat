333076351e8569642c39cabaeba34066e8f7ae86
zed <zed@893a96be-7f27-4fdf-9d1e-6aeec9d3cce1>
2003-06-12 05:30:36 +0000

    add large file support
    git-svn-id: https://xchat.svn.sourceforge.net/svnroot/xchat@350 893a96be-7f27-4fdf-9d1e-6aeec9d3cce1

--- src/fe-gtk/gtkutil.c
+++ src/fe-gtk/gtkutil.c
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
  */
-
+#define _FILE_OFFSET_BITS 64 /* allow selection of large files */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
