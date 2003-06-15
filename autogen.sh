6c881eebec4494d74b218f47873f0f366784eebd
zed <zed@893a96be-7f27-4fdf-9d1e-6aeec9d3cce1>
2003-06-15 05:06:16 +0000

    detect old automake
    git-svn-id: https://xchat.svn.sourceforge.net/svnroot/xchat@358 893a96be-7f27-4fdf-9d1e-6aeec9d3cce1

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,19 @@
+have_automake=false
+if automake --version < /dev/null > /dev/null 2>&1 ; then
+	automake_version=`automake --version | grep 'automake (GNU automake)' | sed 's/^[^0-9]*\(.*\)/\1/'`
+	case $automake_version in
+	   1.2*|1.3*|1.4|1.4*)
+		;;
+	   *)
+		have_automake=true
+		;;
+	esac
+fi
+if $have_automake ; then : ; else
+	echo "automake missing or too old. This requires atleast automake 1.5"
+	exit 1
+fi
+
 echo running aclocal...
 aclocal
 echo running libtoolize...
@@ -17,6 +33,7 @@ automake -a --foreign plugins/python/Makefile
 echo running autoconf...
 autoconf
 echo if no errors occured, run ./configure --enable-maintainer-mode
+exit 0
 
 #autogen.sh generates:
 #	aclocal.m4 Makefile.in config.guess config.sub ltmain.sh
