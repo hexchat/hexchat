f98da524fa02d282dda3a45511ff98319470ea4c
zed <zed@893a96be-7f27-4fdf-9d1e-6aeec9d3cce1>
2003-06-15 16:55:10 +0000

    run automake only once
    git-svn-id: https://xchat.svn.sourceforge.net/svnroot/xchat@369 893a96be-7f27-4fdf-9d1e-6aeec9d3cce1

--- autogen.sh
+++ autogen.sh
@@ -18,18 +18,8 @@ echo running aclocal...
 aclocal
 echo running libtoolize...
 libtoolize
-echo running automake in src...
-automake -a --foreign Makefile
-automake -a --foreign src/Makefile
-automake -a --foreign src/common/Makefile
-automake -a --foreign src/fe-gtk/Makefile
-automake -a --foreign src/fe-text/Makefile
-automake -a --foreign src/pixmaps/Makefile
-echo running automake in plugins...
-automake -a --foreign plugins/Makefile
-automake -a --foreign plugins/tcl/Makefile
-automake -a --foreign plugins/perl/Makefile
-automake -a --foreign plugins/python/Makefile
+echo running automake...
+automake -a --foreign
 echo running autoconf...
 autoconf
 echo if no errors occured, run ./configure --enable-maintainer-mode
