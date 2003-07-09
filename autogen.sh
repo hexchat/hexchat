d4d5fae866b17f289fd9a2c13acfd28562ab6840
zed <zed@893a96be-7f27-4fdf-9d1e-6aeec9d3cce1>
2003-07-09 07:52:26 +0000

    Remove whole intl/ dir. autogen.sh: copy intl from local gettext installation.
    git-svn-id: https://xchat.svn.sourceforge.net/svnroot/xchat@406 893a96be-7f27-4fdf-9d1e-6aeec9d3cce1

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 have_automake=false
+
 if automake --version < /dev/null > /dev/null 2>&1 ; then
 	automake_version=`automake --version | grep 'automake (GNU automake)' | sed 's/^[^0-9]*\(.*\)/\1/'`
 	case $automake_version in
@@ -10,10 +11,59 @@ if automake --version < /dev/null > /dev/null 2>&1 ; then
 		;;
 	esac
 fi
-if $have_automake ; then : ; else
-	echo "automake missing or too old. This requires atleast automake 1.5"
-	exit 1
+if $have_automake ; then
+	AUTOMAKE="automake"
+else
+	if automake-1.7 --version < /dev/null > /dev/null 2>&1 ; then
+		AUTOMAKE="automake-1.7"
+	else
+		echo "automake missing or too old. This requires atleast automake 1.5"
+		exit 1
+	fi
 fi
+
+# ------ START GETTEXT ------
+
+echo searching for GNU gettext intl directory...
+
+dirs="/usr/share /usr/local/share /opt/share /usr /usr/local /opt /usr/gnu/share"
+found=0
+for try in $dirs; do
+	echo -n " -> $try/gettext/intl... "
+	if test -d $try/gettext/intl; then
+		echo found it
+		found=1
+		break
+	fi
+	echo no
+done
+if test "$found" != 1; then
+	echo ERROR: Cannot find gettext/intl directory.
+	echo ERROR: Install GNU gettext in /usr or /usr/local prefix.
+	exit 6
+fi;
+
+echo copying gettext intl files...
+intldir="$try/gettext/intl"
+if test ! -d intl; then
+	mkdir intl
+fi
+olddir=`pwd`
+cd $intldir
+for file in *; do
+	if test $file != COPYING.LIB-2.0 && test $file != COPYING.LIB-2.1; then
+		rm -f $olddir/intl/$file
+		cp $intldir/$file $olddir/intl/
+	fi
+done
+cd $olddir
+if test -f intl/plural.c; then
+	sleep 2
+	touch intl/plural.c
+fi
+
+# ------ END GETTEXT ------
+
 
 echo running aclocal...
 aclocal
@@ -27,8 +77,8 @@ if test "$?" != "0"; then
 	echo libtoolize failed, stopping.
 	exit 3
 fi
-echo running automake...
-automake -a --foreign
+echo running $AUTOMAKE...
+$AUTOMAKE -a --foreign
 if test "$?" != "0"; then
 	echo automake failed, stopping.
 	exit 4
@@ -39,6 +89,7 @@ if test "$?" != "0"; then
 	echo autoconf failed, stopping.
 	exit 5
 fi
+
 echo if no errors occured, run ./configure --enable-maintainer-mode
 exit 0
 
