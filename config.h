d2db3c24dac0505c6179b81748e6ca43d2db6799
berkeviktor@aol.com <berkeviktor@aol.com>
2010-04-18 17:39:35 +0200

    fix scrollback. thanks to a contributor.

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 #define USE_GMODULE
 #define USE_PLUGIN
 #define PACKAGE_NAME "xchat"
-#define PACKAGE_VERSION "r1409-8"
+#define PACKAGE_VERSION "r1409-9"
 #define XCHATLIBDIR "."
 #define XCHATSHAREDIR "."
 #define OLD_PERL
