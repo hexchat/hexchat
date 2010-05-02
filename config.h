8de1804e93ed711cd5f0a2675ec092dd8cb24a39
berkeviktor@aol.com <berkeviktor@aol.com>
2010-05-02 15:14:16 +0200

    update version number

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 #define USE_GMODULE
 #define USE_PLUGIN
 #define PACKAGE_NAME "xchat"
-#define PACKAGE_VERSION "r1409-10"
+#define PACKAGE_VERSION "r1412"
 #define XCHATLIBDIR "."
 #define XCHATSHAREDIR "."
 #define OLD_PERL
