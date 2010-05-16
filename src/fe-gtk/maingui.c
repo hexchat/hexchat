0b74b152273c3f9a08daa4d653c642457a5dda11
zed <zed@893a96be-7f27-4fdf-9d1e-6aeec9d3cce1>
2010-05-16 03:20:21 +0000

    Remove GTK_DISABLE_DEPRECATED, as this only ensured future breakage.
    git-svn-id: https://xchat.svn.sourceforge.net/svnroot/xchat@1423 893a96be-7f27-4fdf-9d1e-6aeec9d3cce1

--- src/fe-gtk/maingui.c
+++ src/fe-gtk/maingui.c
@@ -16,8 +16,6 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
  */
 
-#define GTK_DISABLE_DEPRECATED
-
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
--- src/fe-gtk/palette.c
+++ src/fe-gtk/palette.c
@@ -16,8 +16,6 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
  */
 
-#define GTK_DISABLE_DEPRECATED
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
