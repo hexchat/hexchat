ff21b26a9bab81630eb7deeb6f2e2df4b4961cb3
berkeviktor@aol.com <berkeviktor@aol.com>
2010-05-21 21:50:49 +0200

    update XChat to r1431

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 #define USE_GMODULE
 #define USE_PLUGIN
 #define PACKAGE_NAME "xchat"
-#define PACKAGE_VERSION "r1412-3"
+#define PACKAGE_VERSION "r1431"
 #define XCHATLIBDIR "."
 #define XCHATSHAREDIR "."
 #define OLD_PERL
