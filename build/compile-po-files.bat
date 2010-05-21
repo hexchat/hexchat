59463104a086429dfbea3a8b82154ca74bc4e250
berkeviktor@aol.com <berkeviktor@aol.com>
2010-05-21 22:02:46 +0200

    include translations added since 2.8.6

--- build/compile-po-files.bat
+++ build/compile-po-files.bat
@@ -5,8 +5,17 @@ cd ..\po
 rmdir /Q /S locale
 mkdir locale
 
+mkdir locale\am\LC_MESSAGES
+msgfmt -cvo locale\be\LC_MESSAGES\xchat.mo am.po
+
+mkdir locale\az\LC_MESSAGES
+msgfmt -cvo locale\be\LC_MESSAGES\xchat.mo az.po
+
 mkdir locale\be\LC_MESSAGES
 msgfmt -cvo locale\be\LC_MESSAGES\xchat.mo be.po
+
+mkdir locale\bg\LC_MESSAGES
+msgfmt -cvo locale\be\LC_MESSAGES\xchat.mo bg.po
 
 mkdir locale\ca\LC_MESSAGES
 msgfmt -cvo locale\ca\LC_MESSAGES\xchat.mo ca.po
@@ -20,8 +29,17 @@ msgfmt -cvo locale\de\LC_MESSAGES\xchat.mo de.po
 mkdir locale\el\LC_MESSAGES
 msgfmt -cvo locale\el\LC_MESSAGES\xchat.mo el.po
 
+mkdir locale\en_GB\LC_MESSAGES
+msgfmt -cvo locale\be\LC_MESSAGES\xchat.mo en_GB.po
+
 mkdir locale\es\LC_MESSAGES
 msgfmt -cvo locale\es\LC_MESSAGES\xchat.mo es.po
+
+mkdir locale\et\LC_MESSAGES
+msgfmt -cvo locale\be\LC_MESSAGES\xchat.mo et.po
+
+mkdir locale\eu\LC_MESSAGES
+msgfmt -cvo locale\be\LC_MESSAGES\xchat.mo eu.po
 
 mkdir locale\fi\LC_MESSAGES
 msgfmt -cvo locale\fi\LC_MESSAGES\xchat.mo fi.po
@@ -32,6 +50,9 @@ msgfmt -cvo locale\fr\LC_MESSAGES\xchat.mo fr.po
 mkdir locale\gl\LC_MESSAGES
 msgfmt -cvo locale\gl\LC_MESSAGES\xchat.mo gl.po
 
+mkdir locale\hi\LC_MESSAGES
+msgfmt -cvo locale\be\LC_MESSAGES\xchat.mo hi.po
+
 mkdir locale\hu\LC_MESSAGES
 msgfmt -cvo locale\hu\LC_MESSAGES\xchat.mo hu.po
 
@@ -41,26 +62,53 @@ msgfmt -cvo locale\it\LC_MESSAGES\xchat.mo it.po
 mkdir locale\ja\LC_MESSAGES
 msgfmt -cvo locale\ja\LC_MESSAGES\xchat.mo ja.po
 
+mkdir locale\kn\LC_MESSAGES
+msgfmt -cvo locale\be\LC_MESSAGES\xchat.mo kn.po
+
 mkdir locale\ko\LC_MESSAGES
 msgfmt -cvo locale\ko\LC_MESSAGES\xchat.mo ko.po
 
 mkdir locale\lt\LC_MESSAGES
 msgfmt -cvo locale\lt\LC_MESSAGES\xchat.mo lt.po
 
+mkdir locale\lv\LC_MESSAGES
+msgfmt -cvo locale\be\LC_MESSAGES\xchat.mo lv.po
+
 mkdir locale\mk\LC_MESSAGES
 msgfmt -cvo locale\mk\LC_MESSAGES\xchat.mo mk.po
+
+mkdir locale\ms\LC_MESSAGES
+msgfmt -cvo locale\be\LC_MESSAGES\xchat.mo ms.po
+
+mkdir locale\nb\LC_MESSAGES
+msgfmt -cvo locale\be\LC_MESSAGES\xchat.mo nb.po
 
 mkdir locale\nl\LC_MESSAGES
 msgfmt -cvo locale\nl\LC_MESSAGES\xchat.mo nl.po
 
+mkdir locale\no\LC_MESSAGES
+msgfmt -cvo locale\be\LC_MESSAGES\xchat.mo no.po
+
 mkdir locale\pa\LC_MESSAGES
 msgfmt -cvo locale\pa\LC_MESSAGES\xchat.mo pa.po
+
+mkdir locale\pl\LC_MESSAGES
+msgfmt -cvo locale\be\LC_MESSAGES\xchat.mo pl.po
 
 mkdir locale\pt\LC_MESSAGES
 msgfmt -cvo locale\pt\LC_MESSAGES\xchat.mo pt.po
 
+mkdir locale\pt_BR\LC_MESSAGES
+msgfmt -cvo locale\be\LC_MESSAGES\xchat.mo pt_BR.po
+
 mkdir locale\ru\LC_MESSAGES
 msgfmt -cvo locale\ru\LC_MESSAGES\xchat.mo ru.po
+
+mkdir locale\sk\LC_MESSAGES
+msgfmt -cvo locale\be\LC_MESSAGES\xchat.mo sk.po
+
+mkdir locale\sl\LC_MESSAGES
+msgfmt -cvo locale\be\LC_MESSAGES\xchat.mo sl.po
 
 mkdir locale\sq\LC_MESSAGES
 msgfmt -cvo locale\sq\LC_MESSAGES\xchat.mo sq.po
