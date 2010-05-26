df3a27519e37a7582689e9f100ae8c046598cf1c
berkeviktor@aol.com <berkeviktor@aol.com>
2010-05-26 04:03:28 +0200

    disable transparent background option since it causes a freeze

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 #define USE_GMODULE
 #define USE_PLUGIN
 #define PACKAGE_NAME "xchat"
-#define PACKAGE_VERSION "r1431-3"
+#define PACKAGE_VERSION "r1431-4"
 #define XCHATLIBDIR "."
 #define XCHATSHAREDIR "."
 #define OLD_PERL
