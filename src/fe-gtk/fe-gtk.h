52028eccc2ed201960c639e7b92737035379793e
zed <zed@893a96be-7f27-4fdf-9d1e-6aeec9d3cce1>
2010-05-30 06:31:28 +0000

    === Bump version to 2.8.8 ===
    git-svn-id: https://xchat.svn.sourceforge.net/svnroot/xchat@1439 893a96be-7f27-4fdf-9d1e-6aeec9d3cce1

--- src/fe-gtk/fe-gtk.h
+++ src/fe-gtk/fe-gtk.h
@@ -2,9 +2,9 @@
 
 #ifdef WIN32
 /* If you're compiling this for Windows, your release is un-official
- * and not condoned. Please don't use the XChat name. Call it YChat
- * or make up your own name! */
-#define DISPLAY_NAME "YChat"
+ * and not condoned. Please don't use the XChat name. Make up your
+ * own name! */
+#define DISPLAY_NAME "XChat-Unofficial"
 #else
 #define DISPLAY_NAME "XChat"
 #endif
