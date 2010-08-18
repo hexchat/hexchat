562d7f6e441ba82410a9a066e7831b380e9ec8b5
berkeviktor@aol.com <berkeviktor@aol.com>
2010-08-19 00:04:11 +0200

    update xchat to r1459

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
 #define USE_GTKSPELL
 #endif*/
 #define PACKAGE_NAME "xchat"
-#define PACKAGE_VERSION "r1457"
+#define PACKAGE_VERSION "r1459"
 #define XCHATLIBDIR "."
 #define XCHATSHAREDIR "."
 #define OLD_PERL
