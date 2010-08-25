db33377a783db44a464588a091ba456087309996
lsitu <lsitu@893a96be-7f27-4fdf-9d1e-6aeec9d3cce1>
2010-08-25 02:13:41 +0000

    add entries to build for 5.10.1 and 5.12.1 as well
    git-svn-id: https://xchat.svn.sourceforge.net/svnroot/xchat@1462 893a96be-7f27-4fdf-9d1e-6aeec9d3cce1

--- plugins/perl/xcperl_build.bat
+++ plugins/perl/xcperl_build.bat
@@ -1,6 +1,6 @@
 perl generate_header
 
-gcc -W -Os -DPACKAGE_VERSION=\"2.8.6\" -DWIN32 -DOLD_PERL -DPERL_DLL=\"perl58.dll\" -I "C:\MinGW\include" -I .. -I "C:\ActivePerl-5.8.9\perl\lib\CORE" -L "C:\ActivePerl-5.8.9\perl\bin" -c perl.c -o perl5.8.9.o
+gcc -W -Os -DWIN32 -DPERL_DLL=\"perl58.dll\" -I "C:\MinGW\include" -I .. -I "C:\ActivePerl-5.8.9\perl\lib\CORE" -L "C:\ActivePerl-5.8.9\perl\bin" -c perl.c -o perl5.8.9.o
 
 dllwrap --def perl.def --dllname xcperl5.8.9.dll "C:\ActivePerl-5.8.9\perl\bin\perl58.dll" perl5.8.9.o
 
@@ -8,8 +8,23 @@ strip xcperl5.8.9.dll
 
 
 
-gcc -W -Os -DPACKAGE_VERSION=\"2.8.6\" -DWIN32 -DOLD_PERL -DPERL_DLL=\"perl510.dll\" -I "C:\MinGW\include" -I .. -I "C:\Perl\lib\CORE" -L "C:\Perl\bin" -c perl.c -o perl5.10.0.o
+gcc -W -Os -DWIN32 -DPERL_DLL=\"perl510.dll\" -I "C:\MinGW\include" -I .. -I "C:\Perl\lib\CORE" -L "C:\Perl\bin" -c perl.c -o perl5.10.0.o
 
 dllwrap --def perl.def --dllname xcperl5.10.0.dll "C:\Perl\bin\perl510.dll" perl5.10.0.o
 
 strip xcperl5.10.0.dll
+
+
+gcc -W -Os -DWIN32 -DPERL_DLL=\"perl510.dll\" -I "C:\MinGW\include" -I .. -I "C:\ActivePerl-5.10.1\perl\lib\CORE" -L "C:\ActivePerl-5.10.1\perl\bin" -c perl.c -o perl5.10.1.o
+
+dllwrap --def perl.def --dllname xcperl5.10.1.dll "C:\ActivePerl-5.10.1\perl\bin\perl510.dll" perl5.10.1.o
+
+strip xcperl5.10.1.dll
+
+
+gcc -W -Os -DWIN32 -DPERL_DLL=\"perl58.dll\" -I "C:\MinGW\include" -I .. -I "C:\ActivePerl-5.12.1\perl\lib\CORE" -L "C:\ActivePerl-5.12.1\perl\bin" -c perl.c -o perl5.12.1.o
+
+dllwrap --def perl.def --dllname xcperl5.12.1.dll "C:\ActivePerl-5.12.1\perl\bin\perl512.dll" perl5.12.1.o
+
+strip xcperl5.12.1.dll
+
