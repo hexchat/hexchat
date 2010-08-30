b2ba474f06268afaff76b409ba5790b7db9bbbed
berkeviktor@aol.com <berkeviktor@aol.com>
2010-08-30 10:08:17 +0200

    preliminary update checker

---
+++ src/fe-gtk/check-version.c
@@ -0,0 +1,36 @@
+#include <windows.h>
+#include <wininet.h>
+
+char* check_version ()
+{
+	HINTERNET hINet, hFile;
+	hINet = InternetOpen("XChat-WDK Update Checker", INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0 );
+	
+	if (!hINet)
+	{
+		return "error";
+	}
+
+	hFile = InternetOpenUrl (hINet, "http://xchat-wdk.googlecode.com/hg/version.txt", NULL, 0, 0, 0);
+	
+	if (hFile)
+	{
+		static char buffer[1024];
+		DWORD dwRead;
+		while (InternetReadFile(hFile, buffer, 1023, &dwRead))
+		{
+			if (dwRead == 0)
+			{
+				break;
+			}
+			buffer[dwRead] = 0;
+		}
+		
+		return buffer;
+		InternetCloseHandle (hFile);
+	}
+	
+	InternetCloseHandle (hINet);
+
+	return "error";
+}
---
+++ src/fe-gtk/check-version.h
@@ -0,0 +1,1 @@
+char* check_version ();
