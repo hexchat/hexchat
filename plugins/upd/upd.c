a8dce85bbbc67f41d47b30666e8f05874280ca30
berkeviktor@aol.com <berkeviktor@aol.com>
2010-09-07 02:58:18 +0200

    command to reflect name change

--- plugins/upd/upd.c
+++ plugins/upd/upd.c
@@ -95,7 +95,7 @@ xchat_plugin_init (xchat_plugin *plugin_handle, char **plugin_name, char **plugi
 	*plugin_desc = "Plugin for checking for XChat-WDK updates";
 	*plugin_version = "1.0";
 
-	xchat_hook_command (ph, "UPD", XCHAT_PRI_NORM, print_version, 0, 0);
+	xchat_hook_command (ph, "UPDCHK", XCHAT_PRI_NORM, print_version, 0, 0);
 	xchat_print (ph, "XChat-WDK Update Checker plugin loaded\n");
 	print_version ();
 
