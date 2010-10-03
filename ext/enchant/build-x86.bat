c7fd1a5022ba0ae126faace39ee539ea4c5cf94b
berkeviktor@aol.com <berkeviktor@aol.com>
2010-10-03 03:32:51 +0200

    enable ispell support, it won't hurt

--- ext/enchant/build-x86.bat
+++ ext/enchant/build-x86.bat
@@ -1,6 +1,6 @@
 @echo off
 set PATH=c:\mozilla-build\mingw32\bin;c:\mozilla-build\msys\bin;c:\mozilla-build\build\xchat-dev32\bin
-sh configure --exec-prefix=/usr/enchant-1.5.0-x86 --enable-myspell --disable-ispell --disable-aspell --disable-voikko --disable-uspell --disable-hspell --disable-zemberek
+sh configure --exec-prefix=/usr/enchant-1.5.0-x86 --enable-myspell --enable-ispell --disable-aspell --disable-voikko --disable-uspell --disable-hspell --disable-zemberek
 pause
 make
 pause
