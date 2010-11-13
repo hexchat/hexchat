b90612ef441b4735d3583971c69b38c6cde4b6d8
berkeviktor@aol.com <berkeviktor@aol.com>
2010-11-13 08:32:47 +0100

    close tsvn dialog after export

--- build/release-src.bat
+++ build/release-src.bat
@@ -2,7 +2,7 @@
 set PATH=%PATH%;%PROGRAMW6432%\7-Zip;%PROGRAMW6432%\TortoiseSVN\bin;%PROGRAMFILES(X86)%\TortoiseHg
 hg clone https://xchat-wdk.googlecode.com/hg/ xchat-wdk
 rmdir /q /s xchat-wdk\.hg
-TortoiseProc /closeonend:0 /notempfile /command:export /url:https://xchat.svn.sourceforge.net/svnroot/xchat /path:xchat-wdk-svn
+TortoiseProc /closeonend:1 /notempfile /command:export /url:https://xchat.svn.sourceforge.net/svnroot/xchat /path:xchat-wdk-svn
 rm xchat-wdk-svn\COPYING
 xcopy /E xchat-wdk-svn xchat-wdk
 rmdir /q /s xchat-wdk-svn
