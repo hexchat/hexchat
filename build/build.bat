f16af8be941b596dedac3bf4e371ee2d21f4b598
berkeviktor@aol.com <berkeviktor@aol.com>
2011-02-24 04:13:14 +0100

    nuke the repo

--- build/build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set WDK_ROOT=c:\WinDDK\7600.16385.1
-call build-x86.bat
-call build-x64.bat
-set PATH=%PROGRAMFILES(X86)%\Inno Setup 5
-compil32 /cc xchat-wdk.iss
-pause
--- build/deb-extract.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-for %%A in (*.deb) do 7z x %%A && 7z x data.tar && del data.tar
