08ce6280df217b0a9673c2b91b5400bea45387c9
viktor@asus <viktor@asus>
2011-08-07 07:12:10 +0200

    fix another mysterious ifdef which breaks gtkspell usage on windows

--- src/fe-gtk/fkeys.c
+++ src/fe-gtk/fkeys.c
@@ -314,7 +314,7 @@ key_handle_key_press (GtkWidget *wid, GdkEventKey *evt, session *sess)
 		key_action_tab_clean ();
 		break;
 
-#if defined(USE_GTKSPELL) && !defined(WIN32)
+#if defined(USE_GTKSPELL)/* && !defined(WIN32)
 	/* gtktextview has no 'activate' event, so we trap ENTER here */
 	case GDK_Return:
 	case GDK_KP_Enter:
