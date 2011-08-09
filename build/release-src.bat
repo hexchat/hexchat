afbd51dfe2b9c39795efda4e17199efeccc1b05e
Berke Viktor <berkeviktor@aol.com>
2011-08-09 15:48:14 +0200

    minor follow-up fixes

--- build/release-src.bat
+++ build/release-src.bat
@@ -1,7 +1,7 @@
 @echo off
 set PATH=%PATH%;%PROGRAMW6432%\7-Zip;%PROGRAMFILES(X86)%\Git\bin
 git clone --branch wdk https://xchat-wdk.googlecode.com/git/ xchat-wdk
-rmdir /q /s xchat-wdk\.hg
-7z a xchat-wdk-tip.7z xchat-wdk
+rmdir /q /s xchat-wdk\.git
+7z a xchat-wdk-head.7z xchat-wdk
 rmdir /q /s xchat-wdk
 pause
