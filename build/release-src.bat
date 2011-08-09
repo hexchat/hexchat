69bec802b482b35a243424655b4f77de2ac31409
Berke Viktor <berkeviktor@aol.com>
2011-08-09 15:15:13 +0200

    update source releaser script according to git conversion

--- build/release-src.bat
+++ build/release-src.bat
@@ -1,6 +1,6 @@
 @echo off
-set PATH=%PATH%;%PROGRAMW6432%\7-Zip;%PROGRAMFILES(X86)%\TortoiseHg
-hg clone --branch wdk https://xchat-wdk.googlecode.com/hg/ xchat-wdk
+set PATH=%PATH%;%PROGRAMW6432%\7-Zip;%PROGRAMFILES(X86)%\Git\bin
+git clone --branch wdk https://xchat-wdk.googlecode.com/git/ xchat-wdk
 rmdir /q /s xchat-wdk\.hg
 7z a xchat-wdk-tip.7z xchat-wdk
 rmdir /q /s xchat-wdk
