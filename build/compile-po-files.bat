e42cccd473ef49392a2b9f025a01802d1d12020e
Berke Viktor <berkeviktor@aol.com>
2011-08-15 06:52:45 +0200

    simplify l10n compiler script

--- build/compile-po-files.bat
+++ build/compile-po-files.bat
@@ -3,5 +3,8 @@ echo.Compiling translations . . .
 cd ..\po
 rmdir /q /s locale
 mkdir locale
-for %%A in (am az be bg ca cs de el en_GB es et eu fi fr gl hi hu it ja kn ko lt lv mk ms nb nl no pa pl pt pt_BR ru sk sl sq sr sv th uk vi wa zh_CN zh_TW) do mkdir locale\%%A\LC_MESSAGES && msgfmt -co locale\%%A\LC_MESSAGES\xchat.mo %%A.po
+for %%A in (*.po) do (
+	mkdir locale\%%~nA\LC_MESSAGES
+	msgfmt -co locale\%%~nA\LC_MESSAGES\xchat.mo %%A
+)
 cd ..\build
