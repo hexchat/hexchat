2c9e9dfc9bcf8b9369bf25d918a03585a2038070
Berke Viktor <berkeviktor@aol.com>
2011-08-15 05:39:26 +0200

    update scripts for gendef usage

--- build/dep-extract.bat
+++ build/dep-extract.bat
@@ -1,5 +1,6 @@
 @echo off
-set PATH=%PATH%;%PROGRAMW6432%\7-Zip
+set PATH=%PATH%;%PROGRAMW6432%\7-Zip;c:\mozilla-build\gendef
+
 cd ..\dep-x64
 7z x -y *.rpm
 7z x *.cpio
@@ -7,11 +8,17 @@ del *.cpio
 xcopy /q /s /i usr\x86_64-w64-mingw32\sys-root\mingw\* .
 rmdir /q /s usr
 set OPATH=%PATH%
-set PATH=%PATH%;c:\mozilla-build\python-2.7-x64
-python ..\build\convert-lib-x64.py
+set PATH=%PATH%;c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\amd64
+cd lib
+for %%A in (..\bin\*.dll) do (
+	gendef %%A
+	lib /nologo /machine:x64 /def:%%~nA.def
+)
+cd ..
 set PATH=%OPATH%
 7z x *.7z
 copy /y ..\build\glibconfig-x64.h lib\glib-2.0\include\glibconfig.h
+
 cd ..\dep-x86
 7z x -y *.rpm
 7z x *.cpio
@@ -19,8 +26,13 @@ del *.cpio
 xcopy /q /s /i usr\i686-w64-mingw32\sys-root\mingw\* .
 rmdir /q /s usr
 set OPATH=%PATH%
-set PATH=%PATH%;c:\mozilla-build\python-2.7-x86
-python ..\build\convert-lib-x86.py
+set PATH=%PATH%;c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin;c:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE
+cd lib
+for %%A in (..\bin\*.dll) do (
+	gendef ..\bin\%%A
+	lib /nologo /machine:x86 /def:%%~nA.def
+)
+cd ..
 set PATH=%OPATH%
 7z x *.7z
 copy /y ..\build\glibconfig-x86.h lib\glib-2.0\include\glibconfig.h
