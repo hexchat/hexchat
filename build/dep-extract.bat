f105ae7324c1477c345ff9ad7323d86e4eb484a0
Berke Viktor <berkeviktor@aol.com>
2011-11-22 18:43:41 +0100

    remove gettext l10n

--- build/dep-extract.bat
+++ build/dep-extract.bat
@@ -3,7 +3,13 @@ set PATH=%PROGRAMW6432%\7-Zip
 cd ..\dep-x64
 7z -y x *.zip
 7z x *.7z
-cd ..\dep-x86
+cd share\locale
+del /q /s gettext-tools.mo
+del /q /s gettext-runtime.mo
+cd ..\..\..\dep-x86
 7z -y x *.zip
 7z x *.7z
+cd share\locale
+del /q /s gettext-tools.mo
+del /q /s gettext-runtime.mo
 pause
