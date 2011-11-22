3a40480d8fba4cd11cd1f85793c7807749358c8b
Berke Viktor <berkeviktor@aol.com>
2011-11-22 15:12:56 +0100

    some more updates to obs stack

--- build/release-x86.bat
+++ build/release-x86.bat
@@ -11,6 +11,7 @@ copy ..\src\gtk2-prefs\gtk2-prefs.exe %XCHAT_DEST%
 copy %DEPS_ROOT%\bin\libatk-1.0-0.dll %XCHAT_DEST%
 copy %DEPS_ROOT%\bin\libcairo-2.dll %XCHAT_DEST%
 copy %DEPS_ROOT%\bin\libexpat-1.dll %XCHAT_DEST%
+copy %DEPS_ROOT%\bin\libffi-5.dll %XCHAT_DEST%
 copy %DEPS_ROOT%\bin\libfontconfig-1.dll %XCHAT_DEST%
 copy %DEPS_ROOT%\bin\libfreetype-6.dll %XCHAT_DEST%
 copy %DEPS_ROOT%\bin\libgdk_pixbuf-2.0-0.dll %XCHAT_DEST%
