5bb6ba581c430e917ddfdff5543278d2ea89c2ad
Berke Viktor <berkeviktor@aol.com>
2011-11-23 02:16:56 +0100

    remove patched glibconfig, build, build-small, wix installer, advanced installer projects and old inno setup installer

--- build/build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set WDK_ROOT=c:\WinDDK\7600.16385.1
-set INCLUDE=%WDK_ROOT%\inc\api;%WDK_ROOT%\inc\crt;%WDK_ROOT%\inc\api\crt\stl70;%WDK_ROOT%\inc\mfc42;%WDK_ROOT%\inc\ddk;%WDK_ROOT%\inc\api\dao360
-call build-x86.bat
-call build-x64.bat
-::call compress.bat
-set PATH=%PROGRAMFILES(X86)%\Inno Setup 5
-compil32 /cc xchat-wdk.iss
-pause
--- build/installer-x86.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set PATH=C:\Program Files (x86)\Windows Installer XML v3.5\bin
-candle -nologo installer-x86.wxs -ext WixUIExtension -ext WixUtilExtension
-light -nologo installer-x86.wixobj -ext WixUIExtension -ext WixUtilExtension
