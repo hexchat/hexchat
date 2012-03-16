3c983b106b091565ca9cbda41769a20358cba53e
Berke Viktor <berkeviktor@aol.com>
2012-03-16 01:06:14 +0100

    version bump

--- config.h
+++ config.h
@@ -7,7 +7,7 @@
 #define USE_IPV6
 #define HAVE_ISO_CODES
 #define PACKAGE_NAME "xchat"
-#define PACKAGE_VERSION "1500"
+#define PACKAGE_VERSION "1503"
 #define XCHAT_RELEASE "2.8.8"
 #define XCHATLIBDIR "."
 #define XCHATSHAREDIR "."
