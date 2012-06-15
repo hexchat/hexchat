d9d94aaf6595acb962271ec32c7a9b3734c6c728
Berke Viktor <berkeviktor@aol.com>
2012-06-15 21:45:34 +0200

    First bunch of cleanups

--- win32/dep-extract-x64.bat
+++ win32/dep-extract-x64.bat
@@ -1,7 +1,7 @@
 @echo off
 set PATH=%PATH%;%PROGRAMW6432%\7-Zip;c:\mozilla-build\gendef
 
-cd deps-x64
+cd x64
 ::obs 7z x -y *.rpm
 ::obs 7z x *.cpio
 ::obs del *.cpio
--- win32/dep-extract-x86.bat
+++ win32/dep-extract-x86.bat
@@ -1,7 +1,7 @@
 @echo off
 set PATH=%PATH%;%PROGRAMW6432%\7-Zip;c:\mozilla-build\gendef
 
-cd deps-x86
+cd Win32
 ::obs 7z x -y *.rpm
 ::obs 7z x *.cpio
 ::obs del *.cpio
