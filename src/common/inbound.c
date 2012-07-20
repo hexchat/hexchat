2aa667c970a7c2905c83e07aaedef187d84be5e3
voruse <voruse@hush.ai>
2012-07-20 18:57:04 -0400

    fix mistaken comparison, this is an assignment

--- src/common/inbound.c
+++ src/common/inbound.c
@@ -747,7 +747,7 @@ inbound_quit (server *serv, char *nick, char *ip, char *reason)
 {
 	GSList *list = sess_list;
 	session *sess;
-	struct User *user = NULL;
+	struct User *user;
 	int was_on_front_session = FALSE;
 
 	while (list)
@@ -757,7 +757,7 @@ inbound_quit (server *serv, char *nick, char *ip, char *reason)
 		{
  			if (sess == current_sess)
  				was_on_front_session = TRUE;
-			if (user == userlist_find (sess, nick))
+			if ((user = userlist_find (sess, nick)))
 			{
 				EMIT_SIGNAL (XP_TE_QUIT, sess, nick, reason, ip, NULL, 0);
 				userlist_remove_user (sess, user);
