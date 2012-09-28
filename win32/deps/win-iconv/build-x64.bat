f816bd4d715727f9b0617a46265fec26b41f0448
Berke Viktor <bviktor@hexchat.org>
2012-09-28 18:05:49 +0200

    Add win-iconv build scripts

---
+++ win32/deps/win-iconv/build-x64.bat
@@ -0,0 +1,27 @@
+:: run this from a VS x64 command prompt
+@echo off
+
+SET PACKAGE_NAME=win-iconv-0.0.4
+
+set ICONV_SRC=%cd%
+set ICONV_DEST=%cd%-x64
+del CMakeCache.txt
+rmdir /q /s CMakeFiles
+cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%ICONV_DEST% -DCMAKE_BUILD_TYPE=Release
+nmake clean
+nmake
+echo.Press return when ready to install!
+pause
+
+nmake install
+nmake clean
+
+cd %ICONV_DEST%
+set PATH=%PATH%;%ProgramFiles%\7-zip
+del ..\%PACKAGE_NAME%-x64.7z
+7z a ..\%PACKAGE_NAME%-x64.7z *
+cd %ICONV_SRC%
+rmdir /q /s %ICONV_DEST%
+
+echo.Finished!
+pause
---
+++ win32/deps/win-iconv/build-x86.bat
@@ -0,0 +1,27 @@
+:: run this from a VS x86 command prompt
+@echo off
+
+SET PACKAGE_NAME=win-iconv-0.0.4
+
+set ICONV_SRC=%cd%
+set ICONV_DEST=%cd%-x86
+del CMakeCache.txt
+rmdir /q /s CMakeFiles
+cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%ICONV_DEST% -DCMAKE_BUILD_TYPE=Release
+nmake clean
+nmake
+echo.Press return when ready to install!
+pause
+
+nmake install
+nmake clean
+
+cd %ICONV_DEST%
+set PATH=%PATH%;%ProgramFiles%\7-zip
+del ..\%PACKAGE_NAME%-x86.7z
+7z a ..\%PACKAGE_NAME%-x86.7z *
+cd %ICONV_SRC%
+rmdir /q /s %ICONV_DEST%
+
+echo.Finished!
+pause
