b3d00a8dc03944010bc9818f343d354d70719786
Berke Viktor <bviktor@hexchat.org>
2012-09-28 22:27:58 +0200

    Add gettext-runtime build scripts and patches

---
+++ win32/deps/gettext-runtime/build-x64.bat
@@ -0,0 +1,28 @@
+:: run this from a VS x64 command prompt
+@echo off
+
+SET PACKAGE_NAME=gettext-runtime-0.18
+
+set GETTEXT_SRC=%cd%
+set GETTEXT_DEST=%cd%-x64
+del CMakeCache.txt
+rmdir /q /s CMakeFiles
+cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%GETTEXT_DEST% -DCMAKE_BUILD_TYPE=Release -DICONV_INCLUDE_DIR=%GETTEXT_SRC%\..\build\x64\include -DICONV_LIBRARIES=%GETTEXT_SRC%\..\build\x64\lib\iconv.lib
+nmake clean
+nmake
+echo.Press return when ready to install!
+pause
+
+nmake install
+nmake clean
+copy COPYING %GETTEXT_DEST%\LICENSE.GETTEXT
+
+cd %GETTEXT_DEST%
+set PATH=%PATH%;%ProgramFiles%\7-zip
+del ..\%PACKAGE_NAME%-x64.7z
+7z a ..\%PACKAGE_NAME%-x64.7z *
+cd %GETTEXT_SRC%
+rmdir /q /s %GETTEXT_DEST%
+
+echo.Finished!
+pause
---
+++ win32/deps/gettext-runtime/build-x86.bat
@@ -0,0 +1,28 @@
+:: run this from a VS x86 command prompt
+@echo off
+
+SET PACKAGE_NAME=gettext-runtime-0.18
+
+set GETTEXT_SRC=%cd%
+set GETTEXT_DEST=%cd%-x86
+del CMakeCache.txt
+rmdir /q /s CMakeFiles
+cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%GETTEXT_DEST% -DCMAKE_BUILD_TYPE=Release -DICONV_INCLUDE_DIR=%GETTEXT_SRC%\..\build\Win32\include -DICONV_LIBRARIES=%GETTEXT_SRC%\..\build\Win32\lib\iconv.lib
+nmake clean
+nmake
+echo.Press return when ready to install!
+pause
+
+nmake install
+nmake clean
+copy COPYING %GETTEXT_DEST%\LICENSE.GETTEXT
+
+cd %GETTEXT_DEST%
+set PATH=%PATH%;%ProgramFiles%\7-zip
+del ..\%PACKAGE_NAME%-x86.7z
+7z a ..\%PACKAGE_NAME%-x86.7z *
+cd %GETTEXT_SRC%
+rmdir /q /s %GETTEXT_DEST%
+
+echo.Finished!
+pause
