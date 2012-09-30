d6c13e0f89be3aacadc9d52efe1a82ccaee361c3
Berke Viktor <bviktor@hexchat.org>
2012-09-30 21:41:34 +0200

    Add ATK solution and release scripts

---
+++ win32/deps/atk/release-x86.bat
@@ -0,0 +1,21 @@
+:: run this from a command prompt
+@echo off
+
+SET PACKAGE_NAME=atk-2.5.91
+
+set ATK_SRC=%cd%
+set ATK_DEST=%cd%-rel
+echo.Press return when ready to install!
+pause
+
+copy COPYING %ATK_DEST%\LICENSE.ATK
+
+cd %ATK_DEST%
+set PATH=%PATH%;%ProgramFiles%\7-zip
+del ..\%PACKAGE_NAME%-x86.7z
+7z a ..\%PACKAGE_NAME%-x86.7z *
+cd %ATK_SRC%
+rmdir /q /s %ATK_DEST%
+
+echo.Finished!
+pause
