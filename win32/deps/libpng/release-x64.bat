d168882a32462ed0a6c4f93549549973380e5356
Berke Viktor <bviktor@hexchat.org>
2012-09-30 19:33:46 +0200

    Add libpng solution and release scripts

---
+++ win32/deps/libpng/release-x64.bat
@@ -0,0 +1,34 @@
+:: run this from a VS x64 command prompt
+@echo off
+
+SET PACKAGE_NAME=libpng-1.5.12
+
+set LIBPNG_SRC=%cd%
+set LIBPNG_DEST=%cd%-x64
+echo.Press return when ready to install!
+pause
+
+rmdir /q /s %LIBPNG_DEST%
+mkdir %LIBPNG_DEST%
+mkdir %LIBPNG_DEST%\bin
+mkdir %LIBPNG_DEST%\include
+mkdir %LIBPNG_DEST%\lib
+copy png.h %LIBPNG_DEST%\include
+copy pngconf.h %LIBPNG_DEST%\include
+copy pnglibconf.h %LIBPNG_DEST%\include
+copy pngpriv.h %LIBPNG_DEST%\include
+copy projects\vstudio\x64\Release\libpng15.exp %LIBPNG_DEST%\lib
+copy projects\vstudio\x64\Release\libpng15.lib %LIBPNG_DEST%\lib
+copy projects\vstudio\x64\Release\libpng15.dll %LIBPNG_DEST%\bin
+copy projects\vstudio\x64\Release\pngtest.exe %LIBPNG_DEST%\bin
+copy projects\vstudio\x64\Release\pngvalid.exe %LIBPNG_DEST%\bin
+
+cd %LIBPNG_DEST%
+set PATH=%PATH%;%ProgramFiles%\7-zip
+del ..\%PACKAGE_NAME%-x64.7z
+7z a ..\%PACKAGE_NAME%-x64.7z *
+cd %LIBPNG_SRC%
+rmdir /q /s %LIBPNG_DEST%
+
+echo.Finished!
+pause
