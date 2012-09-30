b192eecee2158810e9f42bb5ece1542750063f22
Berke Viktor <bviktor@hexchat.org>
2012-09-30 18:08:52 +0200

    Note to self

--- win32/deps/libffi/build-x64.bat
+++ win32/deps/libffi/build-x64.bat
@@ -1,5 +1,6 @@
 #run this from mozilla-build\start-msvc11-x64.bat
 #cd /c/mozilla-build/hexchat/libffi-3.0.11
+#TODO: use own zlib build instead of mozilla-build one
 
 ./configure CC="$(pwd)/msvcc.sh -m64" LD=link CPP='cl -nologo -EP' CFLAGS='-O2' --build=x86_64-w64-mingw32
 make clean
--- win32/deps/libffi/build-x86.bat
+++ win32/deps/libffi/build-x86.bat
@@ -1,5 +1,6 @@
 #run this from mozilla-build\start-msvc11.bat
 #cd /c/mozilla-build/hexchat/libffi-3.0.11
+#TODO: use own zlib build instead of mozilla-build one
 
 ./configure CC=$(pwd)/msvcc.sh LD=link CPP='cl -nologo -EP' CFLAGS='-O2' --build=i686-pc-mingw32
 make clean
