149dc7082162ad6a4951ace52b338826a9e19619
Berke Viktor <bviktor@hexchat.org>
2012-10-02 13:53:54 +0200

    Add GTK+ solution and release scripts

---
+++ win32/deps/gtk/release-x64.bat
@@ -0,0 +1,21 @@
+:: run this from a command prompt
+@echo off
+
+SET PACKAGE_NAME=gtk-2.24.13
+
+set GTK_SRC=%cd%
+set GTK_DEST=%cd%-rel
+echo.Press return when ready to install!
+pause
+
+copy COPYING %GTK_DEST%\LICENSE.GTK
+
+cd %GTK_DEST%
+set PATH=%PATH%;%ProgramFiles%\7-zip
+del ..\%PACKAGE_NAME%-x64.7z
+7z a ..\%PACKAGE_NAME%-x64.7z *
+cd %GTK_SRC%
+rmdir /q /s %GTK_DEST%
+
+echo.Finished!
+pause
