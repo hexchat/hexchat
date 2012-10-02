0a68629dae8c78ed75b7ba780a5e71a5967f5b40
Berke Viktor <bviktor@hexchat.org>
2012-10-02 12:59:27 +0200

    Add Pango solution and release scripts

---
+++ win32/deps/pango/release-x86.bat
@@ -0,0 +1,21 @@
+:: run this from a command prompt
+@echo off
+
+SET PACKAGE_NAME=pango-1.30.1
+
+set PANGO_SRC=%cd%
+set PANGO_DEST=%cd%-rel
+echo.Press return when ready to install!
+pause
+
+copy COPYING %PANGO_DEST%\LICENSE.PANGO
+
+cd %PANGO_DEST%
+set PATH=%PATH%;%ProgramFiles%\7-zip
+del ..\%PACKAGE_NAME%-x86.7z
+7z a ..\%PACKAGE_NAME%-x86.7z *
+cd %PANGO_SRC%
+rmdir /q /s %PANGO_DEST%
+
+echo.Finished!
+pause
