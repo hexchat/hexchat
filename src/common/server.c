b59f9abd1a26ef83b0d2496a1170ba498ada8955
Richard Hitt <rbh00@msi.rbh00.pacbell.net>
2012-11-13 12:06:35 -0800

    Limit url-grabbing to NOTICE, PRIVMSG, TOPIC, 332 (RPL_TOPIC), 372 (RPL_MOTD)

--- src/common/server.c
+++ src/common/server.c
@@ -157,6 +157,8 @@ server_send_real (server *serv, char *buf, int len)
 {
 	fe_add_rawlog (serv, buf, len, TRUE);
 
+	url_check_line (buf, len);
+
 	return tcp_send_real (serv->ssl, serv->sok, serv->encoding, serv->using_irc,
 								 buf, len);
 }
