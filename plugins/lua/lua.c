b04377825fb3a6c35c22580295c410f442be5983
Dan Mashal <dan.mashal@fedoraproject.org>
2012-12-23 11:36:54 -0800

    fix incorrect FSF address

--- plugins/lua/lua.c
+++ plugins/lua/lua.c
@@ -13,7 +13,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 /*
  *	$Id: lua.c 91 2007-06-09 18:44:03Z vetinari $
