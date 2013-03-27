8424c32b4a246dddc040114bbe742f72a4477a25
tehsu <tehsu@snyde.net>
2013-03-26 20:34:32 -0500

    Update servlist.c

--- src/common/servlist.c
+++ src/common/servlist.c
@@ -497,9 +497,6 @@ static const struct defaultserver def[] =
 	{0,			"irc.snoonet.org/6667"},
 
 	{"Snyde", 0},
-#ifdef USE_OPENSSL
-	{0,			"irc.snyde.net/+6697"},
-#endif
 	{0,			"irc.snyde.net/6667"},
 
 	{"Sohbet.Net", 0},
