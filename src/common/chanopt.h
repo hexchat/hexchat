3c1bd3cff0a687f817078992488012034fe453bb
Berke Viktor <bviktor@hexchat.org>
2013-03-31 21:44:48 +0200

    Supposed fix for license issues
    I have no clue what to put here so I'll just use what the About dialog
    provides (unless specified otherwise in the file)

--- src/common/chanopt.h
+++ src/common/chanopt.h
@@ -1,3 +1,22 @@
+/* HexChat
+ * Copyright (C) 1998-2010 Peter Zelezny.
+ * Copyright (C) 2009-2013 Berke Viktor.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
 int chanopt_command (session *sess, char *tbuf, char *word[], char *word_eol[]);
 gboolean chanopt_is_set (unsigned int global, guint8 per_chan_setting);
 gboolean chanopt_is_set_a (unsigned int global, guint8 per_chan_setting);
--- src/fe-gtk/joind.c
+++ src/fe-gtk/joind.c
@@ -1,7 +1,22 @@
-/* Copyright (c) 2005 Peter Zelezny
-   All Rights Reserved.
+/* X-Chat
+ * Copyright (C) 2005 Peter Zelezny.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
 
-   joind.c - The Join Dialog.
+/* joind.c - The Join Dialog.
 
    Popups up when you connect without any autojoin channels and helps you
    to find or join a channel.
