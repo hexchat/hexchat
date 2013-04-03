477d7470c1da3fae092687d0dbf5dbab5d37633a
TingPing <tngpng@gmail.com>
2013-04-03 18:35:40 -0300

    renable irc_who_join by default

--- src/common/cfgfiles.c
+++ src/common/cfgfiles.c
@@ -682,7 +682,7 @@ load_config (void)
 	prefs.hex_input_flash_priv = 1;
 	prefs.hex_input_tray_hilight = 1;
 	prefs.hex_input_tray_priv = 1;
-	/* prefs.hex_irc_who_join = 1; prevent kicks and bans caused by overwhelming who'ing after reconnects */
+	prefs.hex_irc_who_join = 1; /* Can kick with inordinate amount of channels, required for some of our features though, TODO: add cap like away check? */
 	prefs.hex_irc_whois_front = 1;
 	prefs.hex_net_auto_reconnect = 1;
 	prefs.hex_net_throttle = 1;
