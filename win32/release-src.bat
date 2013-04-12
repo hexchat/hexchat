30fa189cbb0bcbac07cd7a7c83a309fd79b410c7
Berke Viktor <bviktor@hexchat.org>
2013-04-12 18:05:28 +0200

    We don't release git head anymore, github does that for us

--- win32/release-src.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set PATH=%PATH%;%PROGRAMW6432%\7-Zip;%PROGRAMFILES(X86)%\Git\bin
-git clone https://github.com/hexchat/hexchat.git hexchat
-rmdir /q /s hexchat\.git
-7z a hexchat-head.7z hexchat
-rmdir /q /s hexchat
-pause
