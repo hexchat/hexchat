2550a94f05e51808f22a5803c095fd134d4d81a2
TingPing <tingping@tingping.se>
2013-04-23 00:45:37 -0400

    fix omitting custom sounds

--- src/common/text.c
+++ src/common/text.c
@@ -2187,12 +2187,15 @@ char *sound_files[NUM_XP];
 void
 sound_beep (session *sess)
 {
-	if (sound_files[XP_TE_BEEP] && sound_files[XP_TE_BEEP][0])
-		/* user defined beep _file_ */
-		sound_play_event (XP_TE_BEEP);
-	else
-		/* system beep */
-		fe_beep (sess);
+	if (!prefs.hex_gui_focus_omitalerts || !fe_gui_info (sess, 0) == 1)
+	{
+		if (sound_files[XP_TE_BEEP] && sound_files[XP_TE_BEEP][0])
+			/* user defined beep _file_ */
+			sound_play_event (XP_TE_BEEP);
+		else
+			/* system beep */
+			fe_beep (sess);
+	}
 }
 
 static char *
