33b5dc1c75d5de83c7d49c090266c23961717088
TingPing <tngpng@gmail.com>
2013-06-18 21:20:09 -0300

    Use tabs

--- src/fe-gtk/urlgrab.c
+++ src/fe-gtk/urlgrab.c
@@ -53,14 +53,14 @@ url_treeview_url_clicked_cb (GtkWidget *view, GdkEventButton *event,
 	GtkTreePath *path;
 	GtkTreeView *tree = GTK_TREE_VIEW (view);
 
-   if (!event || !gtk_tree_view_get_path_at_pos (tree, event->x, event->y, &path, 0, 0, 0))
-      return FALSE;
+	if (!event || !gtk_tree_view_get_path_at_pos (tree, event->x, event->y, &path, 0, 0, 0))
+		return FALSE;
 
-   /* select what they right-clicked on */
-   sel = gtk_tree_view_get_selection (tree); 
-   gtk_tree_selection_unselect_all (sel);
-   gtk_tree_selection_select_path (sel, path);
-   gtk_tree_path_free (path); 
+	/* select what they right-clicked on */
+	sel = gtk_tree_view_get_selection (tree); 
+	gtk_tree_selection_unselect_all (sel);
+	gtk_tree_selection_select_path (sel, path);
+	gtk_tree_path_free (path); 
 
 	if (!gtkutil_treeview_get_selected (GTK_TREE_VIEW (view), &iter,
 	                                    URL_COLUMN, &url, -1))
