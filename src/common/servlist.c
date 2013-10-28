6682124cebd355dea7d2700b15fbf5eb850c7756
PoorDog <poordog@openmailbox.org>
2013-10-27 23:39:04 -0400

    Fixed network name
    Changed Techman's-World-IRC to Techman's World IRC as requested by network owner

--- src/common/servlist.c
+++ src/common/servlist.c
@@ -457,7 +457,7 @@ static const struct defaultserver def[] =
 #endif
 	{0,			"irc.swiftirc.net/6667"},
 
-	{"Techman's-World-IRC",		0},
+	{"Techman's World IRC",		0},
 #ifdef USE_OPENSSL
 	{0,			"irc.techmansworld.com/+6697"},
 #endif
