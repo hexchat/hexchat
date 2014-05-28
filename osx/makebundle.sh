c862015f41d7bdc73a63dc98e5a9470931023b23
TingPing <tingping@tingping.se>
2014-05-27 20:35:43 -0400

    osx: makebundle.sh improvements

--- osx/makebundle.sh
+++ osx/makebundle.sh
@@ -11,9 +11,15 @@ if [ ! -f $JHBUILD_PREFIX/bin/python ]; then
 fi
 
 rm -rf HexChat.app
+rm -f *.app.zip
 
 $JHBUILD_PREFIX/bin/python $HOME/.local/bin/gtk-mac-bundler hexchat.bundle
 
 # These take up a lot of space in the bundle
 echo "Cleaning up python files"
-find ./HexChat.app/Contents/Resources/lib/python2.7 -name "*.pyo" -or -name "*.pyc" -delete
+find ./HexChat.app/Contents/Resources/lib/python2.7 -name "*.pyc" -delete
+find ./HexChat.app/Contents/Resources/lib/python2.7 -name "*.pyo" -delete
+
+echo "Compressing bundle"
+#hdiutil create -format UDBZ -srcdir HexChat.app -quiet HexChat-2.9.6.1-$(git rev-parse --short master).dmg
+zip -9rXq ./HexChat-2.9.6.1-$(git rev-parse --short master).app.zip ./HexChat.app 
