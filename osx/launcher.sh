b3bcf71981df984a413d136ff261ac37864f2e4d
TingPing <tingping@tingping.se>
2014-05-31 10:04:05 -0400

    osx: Fix loading system's openssl config

--- osx/launcher.sh
+++ osx/launcher.sh
@@ -40,6 +40,8 @@ export PYTHON="$bundle_contents/MacOS/python"
 export PYTHONHOME="$bundle_res"
 export PYTHONPATH="$bundle_lib/python2.7:$bundle_lib/python2.7/site-packages"
 
+export OPENSSL_CONF="/System/Library/OpenSSL/openssl.cnf"
+
 export HEXCHAT_LIBDIR="$bundle_lib/hexchat/plugins"
 
 APP=name
