ddf68fcdd4b7b4b8316281fc7784ec04e267807b
leeter <leeterfp@gmail.com>
2014-07-18 15:38:53 -0600

    Fix resource leaks in sysinfo and actually use C++ idioms instead of coding C++ like C

--- plugins/sysinfo/sysinfo.cpp
+++ plugins/sysinfo/sysinfo.cpp
@@ -20,31 +20,73 @@
  * THE SOFTWARE.
  */
 
-#include <stdio.h>
+//#include <cstdio>
 #include <windows.h>
 #include <comutil.h>
+#include <comdef.h>
 #include <wbemidl.h>
+#include <string>
+#include <memory>
+#include <vector>
+#include <algorithm>
+#include <locale>
+#include <codecvt>
+#include <sstream>
+#include <chrono>
+#include <mutex>
+#pragma comment(lib, "comsuppw.lib")
 
 #include "hexchat-plugin.h"
+
+
+
+namespace{
+	_COM_SMARTPTR_TYPEDEF(IWbemLocator, __uuidof(IWbemLocator));
+	_COM_SMARTPTR_TYPEDEF(IWbemServices, __uuidof(IWbemServices));
+	_COM_SMARTPTR_TYPEDEF(IEnumWbemClassObject, __uuidof(IEnumWbemClassObject));
+	_COM_SMARTPTR_TYPEDEF(IWbemClassObject, __uuidof(IWbemClassObject));
 
 static hexchat_plugin *ph;   /* plugin handle */
 static char name[] = "SysInfo";
 static char desc[] = "Display info about your hardware and OS";
 static char version[] = "1.1";
 static char helptext[] = "USAGE: /sysinfo - Sends info about your hardware and OS to current channel.";
-static int firstRun;
-static char *wmiOs;
-static char *wmiCpu;
-static char *wmiVga;
+	static bool firstRun;
+	static ::std::string wmiOs;
+	//static char *wmiOs;
+	static ::std::string wmiCpu;
+	static ::std::string wmiVga;
+
+	class coInitialize{
+		const HRESULT res;
+	public:
+		coInitialize(COINIT init)
+			:res(CoInitializeEx(nullptr, init))
+		{}
+		~coInitialize(){
+			if (SUCCEEDED(res)){
+				CoUninitialize();
+			}
+		}
+		operator HRESULT(){
+			return res;
+		}
+	};
+
+	static std::string narrow(const std::wstring& in)
+	{
+		std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t> conversion;
+		return conversion.to_bytes(in);
+	}
 
 static int
-getCpuArch (void)
+		getCpuArch()
 {
-	OSVERSIONINFOEX osvi;
-	SYSTEM_INFO si;
+		OSVERSIONINFOEX osvi{ 0 };
+		SYSTEM_INFO si = { 0 };
 
 	osvi.dwOSVersionInfoSize = sizeof (OSVERSIONINFOEX);
-	GetVersionEx ((LPOSVERSIONINFOW) &osvi);
+		GetVersionEx((LPOSVERSIONINFOW)::std::addressof(osvi));
 
 	GetSystemInfo (&si);
 
@@ -187,49 +229,64 @@ getCpuName (void)
 }
 #endif
 
-static char *
+	struct reg_key{
+		const HKEY key;
+		reg_key(HKEY key)
+			:key(key){};
+		~reg_key(){
+			RegCloseKey(key);
+		}
+	};
+
+	static ::std::string
 getCpuMhz (void)
 {
 	HKEY hKey;
 	int result;
 	int data;
-	int dataSize;
+		DWORD dataSize;
 	double cpuspeed;
-	static char buffer[16];
-	const char *cpuspeedstr;
+		::std::stringstream buffer;
+		//static char buffer[16];
+		//const char *cpuspeedstr;
 
 	if (RegOpenKeyEx (HKEY_LOCAL_MACHINE, TEXT("Hardware\\Description\\System\\CentralProcessor\\0"), 0, KEY_QUERY_VALUE, &hKey) == ERROR_SUCCESS)
 	{
+			reg_key regKey(hKey);
 		dataSize = sizeof (data);
-		result = RegQueryValueEx (hKey, TEXT("~MHz"), 0, 0, (LPBYTE)&data, (LPDWORD)&dataSize);
-		RegCloseKey (hKey);
+			result = RegQueryValueEx(hKey, TEXT("~MHz"), nullptr, nullptr, (LPBYTE)&data, &dataSize);
 		if (result == ERROR_SUCCESS)
 		{
-			cpuspeed = ( data > 1000 ) ? data / 1000 : data;
-			cpuspeedstr = ( data > 1000 ) ? "GHz" : "MHz";
-			sprintf (buffer, "%.2f %s", cpuspeed, cpuspeedstr);
+				cpuspeed = static_cast<double>( (data > 1000) ? data / 1000 : data);
+				buffer << cpuspeed << " " << ((data > 1000) ? "GHz" : "MHz");
 		}
 	}
 
-	return buffer;
+		return buffer.str();
 }
 
-static char *
+	static ::std::string
 getMemoryInfo (void)
 {
-	static char buffer[32];
-	MEMORYSTATUSEX meminfo;
+		::std::stringstream buffer;
+		//static char buffer[32] = { 0 };
+		MEMORYSTATUSEX meminfo = { 0 };
 
 	meminfo.dwLength = sizeof (meminfo);
 	GlobalMemoryStatusEx (&meminfo);
+		buffer << meminfo.ullTotalPhys / 1024 / 1024 << " MB Total (" << meminfo.ullAvailPhys / 1024 / 1024 << " MB Free)";
+		return buffer.str();
+	}
 
-	sprintf (buffer, "%I64d MB Total (%I64d MB Free)", meminfo.ullTotalPhys / 1024 / 1024, meminfo.ullAvailPhys / 1024 / 1024);
+	enum class wmi_info_mode
+	{
+		os,
+		processor,
+		vga
+	};
 
-	return buffer;
-}
-
-static char *
-getWmiInfo (int mode)
+	static ::std::string
+		getWmiInfo(wmi_info_mode mode)
 {
 	/* for more details about this wonderful API, see 
 	http://msdn.microsoft.com/en-us/site/aa394138
@@ -237,25 +294,20 @@ getWmiInfo (int mode)
 	http://msdn.microsoft.com/en-us/library/windows/desktop/aa394138%28v=vs.85%29.aspx
 	http://social.msdn.microsoft.com/forums/en-US/vcgeneral/thread/d6420012-e432-4964-8506-6f6b65e5a451
 	*/
-
-	char *buffer = (char *) malloc (128);
+		coInitialize init(COINIT_APARTMENTTHREADED);
+		//char *buffer = (char *)malloc(128);
 	HRESULT hres;
 	HRESULT hr;
-	IWbemLocator *pLoc = NULL;
-	IWbemServices *pSvc = NULL;
-	IEnumWbemClassObject *pEnumerator = NULL;
-	IWbemClassObject *pclsObj;
-	ULONG uReturn = 0;
+		IWbemLocatorPtr pLoc;
+		IWbemServicesPtr pSvc;
+		IEnumWbemClassObjectPtr pEnumerator;		
 
-	hres =  CoInitializeEx (0, COINIT_APARTMENTTHREADED | COINIT_SPEED_OVER_MEMORY);
-
-	if (FAILED (hres))
+		if (FAILED(init))
 	{
-		strcpy (buffer, "Error Code 0");
-		return buffer;
+			return "Error Code 0";
 	}
 
-	hres =  CoInitializeSecurity (NULL, -1, NULL, NULL, RPC_C_AUTHN_LEVEL_DEFAULT, RPC_C_IMP_LEVEL_IMPERSONATE, NULL, EOAC_NONE, NULL);
+		hres = CoInitializeSecurity(nullptr, -1, nullptr, nullptr, RPC_C_AUTHN_LEVEL_DEFAULT, RPC_C_IMP_LEVEL_IMPERSONATE, nullptr, EOAC_NONE, nullptr);
 
 	/* mysteriously failing after the first execution, but only when used as a plugin, skip it */
 	/*if (FAILED (hres))
@@ -265,126 +317,115 @@ getWmiInfo (int mode)
 		return buffer;
 	}*/
 
-	hres = CoCreateInstance (CLSID_WbemLocator, 0, CLSCTX_INPROC_SERVER, IID_IWbemLocator, (LPVOID *) &pLoc);
+		hres = pLoc.CreateInstance(CLSID_WbemLocator, nullptr, CLSCTX_INPROC_SERVER);
 
 	if (FAILED (hres))
 	{
-		CoUninitialize ();
-		strcpy (buffer, "Error Code 2");
-		return buffer;
+			return "Error Code 2";
 	}
 
-	hres = pLoc->ConnectServer (_bstr_t (L"root\\CIMV2"), NULL, NULL, 0, NULL, 0, 0, &pSvc);
+		hres = pLoc->ConnectServer(_bstr_t(L"root\\CIMV2"), nullptr, nullptr, nullptr, 0, nullptr, nullptr, &pSvc);
 
 	if (FAILED (hres))
 	{
-		pLoc->Release ();
-		CoUninitialize ();
-		strcpy (buffer, "Error Code 3");
-		return buffer;
+			return "Error Code 3";
 	}
 
-	hres = CoSetProxyBlanket (pSvc, RPC_C_AUTHN_WINNT, RPC_C_AUTHZ_NONE, NULL, RPC_C_AUTHN_LEVEL_CALL, RPC_C_IMP_LEVEL_IMPERSONATE, NULL, EOAC_NONE);
+		hres = CoSetProxyBlanket(pSvc, RPC_C_AUTHN_WINNT, RPC_C_AUTHZ_NONE, nullptr, RPC_C_AUTHN_LEVEL_CALL, RPC_C_IMP_LEVEL_IMPERSONATE, nullptr, EOAC_NONE);
 
 	if (FAILED (hres))
 	{
-		pSvc->Release ();
-		pLoc->Release ();
-		CoUninitialize ();
-		strcpy (buffer, "Error Code 4");
-		return buffer;
+			return "Error Code 4";
 	}
 
 	switch (mode)
 	{
-		case 0:
-			hres = pSvc->ExecQuery (_bstr_t ("WQL"), _bstr_t ("SELECT * FROM Win32_OperatingSystem"), WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY, NULL, &pEnumerator);
+		case wmi_info_mode::os:
+			hres = pSvc->ExecQuery(_bstr_t(L"WQL"), _bstr_t(L"SELECT * FROM Win32_OperatingSystem"), WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY, nullptr, &pEnumerator);
 			break;
-		case 1:
-			hres = pSvc->ExecQuery (_bstr_t ("WQL"), _bstr_t ("SELECT * FROM Win32_Processor"), WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY, NULL, &pEnumerator);
+		case wmi_info_mode::processor:
+			hres = pSvc->ExecQuery(_bstr_t(L"WQL"), _bstr_t(L"SELECT * FROM Win32_Processor"), WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY, nullptr, &pEnumerator);
 			break;
-		case 2:
-			hres = pSvc->ExecQuery (_bstr_t ("WQL"), _bstr_t ("SELECT * FROM Win32_VideoController"), WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY, NULL, &pEnumerator);
+		case wmi_info_mode::vga:
+			hres = pSvc->ExecQuery(_bstr_t(L"WQL"), _bstr_t(L"SELECT * FROM Win32_VideoController"), WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY, nullptr, &pEnumerator);
 			break;
 
 	}
 
 	if (FAILED (hres))
 	{
-		pSvc->Release ();
-		pLoc->Release ();
-		CoUninitialize ();
-		strcpy (buffer, "Error Code 5");
-		return buffer;
+			return "Error Code 5";
 	}
-
+		::std::wstringstream buffer;
 	while (pEnumerator)
 	{
+			ULONG uReturn = 0;
+			IWbemClassObjectPtr pclsObj;
 		hr = pEnumerator->Next (WBEM_INFINITE, 1, &pclsObj, &uReturn);
 		if (0 == uReturn)
 		{
 			break;
 		}
-		VARIANT vtProp;
+			_variant_t vtProp;
 		switch (mode)
 		{
-			case 0:
-				hr = pclsObj->Get (L"Caption", 0, &vtProp, 0, 0);
+			case wmi_info_mode::os:
+				hr = pclsObj->Get(L"Caption", 0, &vtProp, nullptr, nullptr);
 				break;
-			case 1:
-				hr = pclsObj->Get (L"Name", 0, &vtProp, 0, 0);
+			case wmi_info_mode::processor:
+				hr = pclsObj->Get(L"Name", 0, &vtProp, nullptr, nullptr);
 				break;
-			case 2:
-				hr = pclsObj->Get (L"Name", 0, &vtProp, 0, 0);
+			case wmi_info_mode::vga:
+				hr = pclsObj->Get(L"Name", 0, &vtProp, nullptr, nullptr);
 				break;
 		}
-		WideCharToMultiByte (CP_ACP, 0, vtProp.bstrVal, -1, buffer, SysStringLen (vtProp.bstrVal)+1, NULL, NULL);
-		VariantClear (&vtProp);
+			buffer<< vtProp.bstrVal;
+		}
+		return narrow(buffer.str());
     }
 
-	pSvc->Release ();
-	pLoc->Release ();
-	pEnumerator->Release ();
-	pclsObj->Release ();
-	CoUninitialize ();
-	return buffer;
-}
+	static std::mutex g_mutex;
 
 static int
 printInfo (char *word[], char *word_eol[], void *user_data)
 {
+		std::chrono::milliseconds ticks(GetTickCount64());
+		using fp_hours = ::std::chrono::duration < float, ::std::chrono::hours::period >;
 	/* query WMI info only at the first time SysInfo is called, then cache it to save time */
 	if (firstRun)
 	{
+			::std::lock_guard<::std::mutex> guard(g_mutex);
 		hexchat_printf (ph, "%s first execution, querying and caching WMI info...\n", name);
-		wmiOs = getWmiInfo (0);
-		wmiCpu = getWmiInfo (1);
-		wmiVga = getWmiInfo (2);
-		firstRun = 0;
+			wmiOs = getWmiInfo(wmi_info_mode::os);
+			wmiCpu = getWmiInfo(wmi_info_mode::processor);
+			wmiVga = getWmiInfo(wmi_info_mode::vga);
+			firstRun = false;
 	}
-	if (hexchat_list_int (ph, NULL, "type") >= 2)
+		if (hexchat_list_int(ph, nullptr, "type") >= 2)
 	{
+			
 		/* uptime will work correctly for up to 50 days, should be enough */
 		hexchat_commandf (ph, "ME ** SysInfo ** Client: HexChat %s (x%d) ** OS: %s ** CPU: %s (%s) ** RAM: %s ** VGA: %s ** Uptime: %.2f Hours **",
 			hexchat_get_info (ph, "version"),
 			getCpuArch (),
-			wmiOs,
-			wmiCpu,
-			getCpuMhz (),
-			getMemoryInfo (),
-			wmiVga, (float) GetTickCount() / 1000 / 60 / 60);
+				wmiOs.c_str(),
+				wmiCpu.c_str(),
+				getCpuMhz().c_str(),
+				getMemoryInfo().c_str(),
+				wmiVga.c_str(), fp_hours(ticks).count());
 	}
 	else
 	{
 		hexchat_printf (ph, " * Client:  HexChat %s (x%d)\n", hexchat_get_info (ph, "version"), getCpuArch ());
-		hexchat_printf (ph, " * OS:      %s\n", wmiOs);
-		hexchat_printf (ph, " * CPU:     %s (%s)\n", wmiCpu, getCpuMhz ());
-		hexchat_printf (ph, " * RAM:     %s\n", getMemoryInfo ());
-		hexchat_printf (ph, " * VGA:     %s\n", wmiVga);
-		hexchat_printf (ph, " * Uptime:  %.2f Hours\n", (float) GetTickCount() / 1000 / 60 / 60);
+			hexchat_printf(ph, " * OS:      %s\n", wmiOs.c_str());
+			hexchat_printf(ph, " * CPU:     %s (%s)\n", wmiCpu.c_str(), getCpuMhz().c_str());
+			hexchat_printf(ph, " * RAM:     %s\n", getMemoryInfo().c_str());
+			hexchat_printf(ph, " * VGA:     %s\n", wmiVga.c_str());
+			hexchat_printf(ph, " * Uptime:  %.2f Hours\n", fp_hours(ticks).count());
 	}
 
 	return HEXCHAT_EAT_HEXCHAT;
+}
 }
 
 int
@@ -396,9 +437,9 @@ hexchat_plugin_init (hexchat_plugin *plugin_handle, char **plugin_name, char **p
 	*plugin_desc = desc;
 	*plugin_version = version;
 
-	firstRun = 1;
+	firstRun = true;
 
-	hexchat_hook_command (ph, "SYSINFO", HEXCHAT_PRI_NORM, printInfo, helptext, NULL);
+	hexchat_hook_command (ph, "SYSINFO", HEXCHAT_PRI_NORM, printInfo, helptext, nullptr);
 	hexchat_command (ph, "MENU -ishare\\system.png ADD \"Window/Send System Info\" \"SYSINFO\"");
 
 	hexchat_printf (ph, "%s plugin loaded\n", name);
