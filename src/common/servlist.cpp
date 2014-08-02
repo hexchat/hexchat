943f641abc91cb424b76a0ac87b1e4a87e4233c9
leeter <leeterfp@gmail.com>
2014-08-01 19:55:07 -0600

    use c++ headers

--- src/common/servlist.cpp
+++ src/common/servlist.cpp
@@ -16,9 +16,9 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
 #include <sys/types.h>
 #include <sys/stat.h>
 
