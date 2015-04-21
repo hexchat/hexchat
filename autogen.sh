9a98de1f65ac031a8d8f1c8ddcb71d3ba5a0d7aa
TingPing <tingping@tingping.se>
2015-04-21 18:34:55 -0400

    Remove requirement on gnome-common
    I'm sure that will make a few users happy..

--- autogen.sh
+++ autogen.sh
@@ -4,19 +4,24 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-NOCONFIGURE=1
-PKG_NAME="hexchat"
-
 (test -f $srcdir/src/common/hexchat.c) || {
-	echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-	echo " top-level $PKG_NAME directory"
+	echo -n "**Error**: Directory "\`$srcdir\'" does not look like the top-level directory"
 	exit 1
 }
 
-which gnome-autogen.sh || {
-	echo "You need to install gnome-common"
-	exit 1
-}
+aclocal --install || exit 1
+glib-gettextize --force --copy || exit 1
+intltoolize --force --copy --automake || exit 1
+autoreconf --force --install -Wno-portability || exit 1
 
-. gnome-autogen.sh
+if [ "$NOCONFIGURE" = "" ]; then
+        $srcdir/configure "$@" || exit 1
 
+        if [ "$1" = "--help" ]; then exit 0 else
+                echo "Now type \`make\' to compile" || exit 1
+        fi
+else
+        echo "Skipping configure process."
+fi
+
+set +x
