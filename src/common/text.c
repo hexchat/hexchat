94ef0fd29480b8390b0adff625122db9e732ac5a
TingPing <tingping@tingping.se>
2015-05-28 14:13:34 -0400

    Fix warning

--- src/common/text.c
+++ src/common/text.c
@@ -2134,7 +2134,7 @@ char *sound_files[NUM_XP];
 void
 sound_beep (session *sess)
 {
-	if (!prefs.hex_gui_focus_omitalerts || !fe_gui_info (sess, 0) == 1)
+	if (!prefs.hex_gui_focus_omitalerts || fe_gui_info (sess, 0) != 1)
 	{
 		if (sound_files[XP_TE_BEEP] && sound_files[XP_TE_BEEP][0])
 			/* user defined beep _file_ */
