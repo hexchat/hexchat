hexchat/hexchat

commit 03767ee0f98262220eb13a2fcb1f2fe0963cba3d

Kill fe_set_hilight()
It just duplicates functionality and this fixes:

- alert_taskbar chanopt not being respected
- tab color being incorrect when highlight print events are eaten

src/fe-gtk/fe-gtk.c
@@ -557,16 +557,6 @@ fe_set_topic (session *sess, char *topic, char *stripped_topic)
 	}
 }
 
-void
-fe_set_hilight (struct session *sess)
-{
-	if (sess->gui->is_tab)
-		fe_set_tab_color (sess, 3);	/* set tab to blue */
-
-	if (prefs.hex_input_flash_hilight && (!prefs.hex_away_omit_alerts || !sess->server->is_away))
-		fe_flash_window (sess); /* taskbar flash */
-}
-
 static void
 fe_update_mode_entry (session *sess, GtkWidget *entry, char **text, char *new_text)
 {

src/common/fe.h
@@ -69,7 +69,6 @@ int fe_input_add (int sok, int flags, void *func, void *data);
 void fe_input_remove (int tag);
 void fe_idle_add (void *func, void *data);
 void fe_set_topic (struct session *sess, char *topic, char *stripped_topic);
-void fe_set_hilight (struct session *sess);
 void fe_set_tab_color (struct session *sess, int col);
 void fe_flash_window (struct session *sess);
 void fe_update_mode_buttons (struct session *sess, char mode, char sign);

src/common/inbound.c
@@ -319,7 +319,6 @@ is_hilight (char *from, char *text, session *sess, server *serv)
 			sess->nick_said = TRUE;
 			lastact_update (sess);
 		}
-		fe_set_hilight (sess);
 		return 1;
 	}
 

src/common/text.c
@@ -2048,6 +2048,7 @@ text_emit (int index, session *sess, char *a, char *b, char *c, char *d,
 	/* ===Highlighted message=== */
 	case XP_TE_HCHANACTION:
 	case XP_TE_HCHANMSG:
+		fe_set_tab_color (sess, 3);
 		if (chanopt_is_set (prefs.hex_input_beep_hilight, sess->alert_beep) && (!prefs.hex_away_omit_alerts || !sess->server->is_away))
 			sound_beep (sess);
 		if (chanopt_is_set (prefs.hex_input_flash_hilight, sess->alert_taskbar) && (!prefs.hex_away_omit_alerts || !sess->server->is_away))

src/fe-text/fe-text.c
@@ -618,10 +618,6 @@ fe_cleanup (void)
 {
 }
 void
-fe_set_hilight (struct session *sess)
-{
-}
-void
 fe_set_tab_color (struct session *sess, int col)
 {
 }

commit e7c88e0f83f40f9d020aa7d2783e95dff55f323f

Fix private highlights not setting tab color
Caused by 03767ee0f98

Fixes #1871

src/fe-gtk/fe-gtk.c
@@ -660,15 +660,20 @@ fe_print_text (struct session *sess, char *text, time_t stamp,
 {
 	PrintTextRaw (sess->res->buffer, (unsigned char *)text, prefs.hex_text_indent, stamp);
 
-	if (!no_activity && !sess->new_data && sess != current_tab &&
-		sess->gui->is_tab && !sess->nick_said)
+	if (no_activity || sess == current_tab || !sess->gui->is_tab)
+		return;
+
+	if (sess->nick_said)
+		fe_set_tab_color (sess, 3);
+	else if (sess->msg_said)
+		fe_set_tab_color (sess, 2);
+	else
+		fe_set_tab_color (sess, 1);
+
+	if (!sess->new_data && !sess->nick_said)
 	{
 		sess->new_data = TRUE;
 		lastact_update (sess);
-		if (sess->msg_said)
-			fe_set_tab_color (sess, 2);
-		else
-			fe_set_tab_color (sess, 1);
 	}
 }
 