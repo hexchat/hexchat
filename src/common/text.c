2d184c09674b417bf294001d017b0d1ad2337de3
Patrick Griffis <tingping@tingping.se>
2017-06-18 06:31:34 -0400

    Fix crash on coloring NULL nicks
    Fixes #2014

--- src/common/text.c
+++ src/common/text.c
@@ -2013,7 +2013,7 @@ text_emit (int index, session *sess, char *a, char *b, char *c, char *d,
 	unsigned int stripcolor_args = (chanopt_is_set (prefs.hex_text_stripcolor_msg, sess->text_strip) ? 0xFFFFFFFF : 0);
 	char tbuf[NICKLEN + 4];
 
-	if (prefs.hex_text_color_nicks && (index == XP_TE_CHANACTION || index == XP_TE_CHANMSG))
+	if (a != NULL && prefs.hex_text_color_nicks && (index == XP_TE_CHANACTION || index == XP_TE_CHANMSG))
 	{
 		g_snprintf (tbuf, sizeof (tbuf), "\003%d%s", text_color_of (a), a);
 		a = tbuf;
