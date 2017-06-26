98ee2eeeb8327070536e091c40b18d1ccc0412eb
Patrick Griffis <tingping@tingping.se>
2017-06-26 19:24:18 -0400

    fixup! Use glib WIN32 defines

--- src/common/hexchat.c
+++ src/common/hexchat.c
@@ -16,6 +16,9 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
+#include "config.h"
+#include <glib.h>
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
