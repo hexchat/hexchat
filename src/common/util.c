02b978167f28da084be75b344e598c338e6cbf91
Joseph Bisch <joseph.bisch@gmail.com>
2017-09-19 07:10:15 -0400

    Fix rfc_ncasecmp handling of different length strings
    If strings are different lengths and at least one is shorter in length
    than n, this now returns the difference between the characters instead
    of always returning 0.

--- src/common/util.c
+++ src/common/util.c
@@ -1040,8 +1040,11 @@ rfc_ncasecmp (char *s1, char *s2, int n)
 {
 	int c1, c2;
 
-	while (*s1 && *s2 && n > 0)
+	while (*s1 && *s2)
 	{
+		if (n == 0)
+			return 0;
+
 		c1 = (int)rfc_tolower (*s1);
 		c2 = (int)rfc_tolower (*s2);
 		if (c1 != c2)
@@ -1050,7 +1053,9 @@ rfc_ncasecmp (char *s1, char *s2, int n)
 		s1++;
 		s2++;
 	}
-	return 0;
+	c1 = (int)rfc_tolower (*s1);
+	c2 = (int)rfc_tolower (*s2);
+	return (c1 - c2);
 }
 
 const unsigned char rfc_tolowertab[] =
