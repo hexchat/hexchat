092c9154c171a08cfd00358163946adbbff0de37
A_D <A_D@snoonet.org>
2018-07-25 21:41:05 +0200

    fixed /py exec behaviour

--- plugins/python/python.py
+++ plugins/python/python.py
@@ -97,7 +97,7 @@ else:
         return compile(data, filename, 'exec', optimize=2, dont_inherit=True)
 
     def compile_line(string):
-        return compile(string, '<string>', 'eval', optimize=2, dont_inherit=True)
+        return compile(string, '<string>', 'single', optimize=2, dont_inherit=True)
 
 
 class Plugin:
