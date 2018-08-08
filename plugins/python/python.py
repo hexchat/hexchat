53a3f07c205a478141b331980b8279b3885efc82
A_D <A_D@snoonet.org>
2018-08-08 02:32:23 +0200

    fixed /exec behaviour (again)

--- plugins/python/python.py
+++ plugins/python/python.py
@@ -97,7 +97,8 @@ else:
         return compile(data, filename, 'exec', optimize=2, dont_inherit=True)
 
     def compile_line(string):
-        return compile(string, '<string>', 'single', optimize=2, dont_inherit=True)
+        # using exec over single as single causes weird behaviour
+        return compile(string, '<string>', 'exec', optimize=2, dont_inherit=True)
 
 
 class Plugin:
