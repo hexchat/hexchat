f3b74fa71c6813b66461b00e2b4dd1191429f438
Stepan Broz <stepan@izitra.cz>
2019-05-21 16:02:30 +0200

    Make dcc_ip being a per-server value. Moved dcc_ip from prefs to sess->server.

--- src/common/inbound.c
+++ src/common/inbound.c
@@ -1420,7 +1420,7 @@ inbound_foundip (session *sess, char *ip, const message_tags_data *tags_data)
 	HostAddr = gethostbyname (ip);
 	if (HostAddr)
 	{
-		prefs.dcc_ip = ((struct in_addr *) HostAddr->h_addr)->s_addr;
+		sess->server->dcc_ip = ((struct in_addr *) HostAddr->h_addr)->s_addr;
 		EMIT_SIGNAL_TIMESTAMP (XP_TE_FOUNDIP, sess->server->server_session,
 									  inet_ntoa (*((struct in_addr *) HostAddr->h_addr)),
 									  NULL, NULL, NULL, 0, tags_data->timestamp);
