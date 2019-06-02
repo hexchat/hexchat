8cd3f5e94fddf2bae9e6b1facfa4e69a803ed4e8
BakasuraRCE <bakasura@protonmail.ch>
2019-06-01 22:50:47 -0500

    fishlim: Add copyright

--- plugins/fishlim/plugin_hexchat.c
+++ plugins/fishlim/plugin_hexchat.c
@@ -2,6 +2,7 @@
 
   Copyright (c) 2010-2011 Samuel Lidén Borell <samuel@kodafritt.se>
   Copyright (c) 2015 <the.cypher@gmail.com>
+  Copyright (c) 2019 <bakasura@protonmail.ch>
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
