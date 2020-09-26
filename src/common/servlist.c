cb06e6c9edac39f1afd95f3a8d67c3f6f41f0588
Harald Ziegler <harald.ziegler@t-online.de>
2020-09-26 13:50:08 +0200

    Added server for ICQ-Chat to servlist.c

--- src/common/servlist.c
+++ src/common/servlist.c
@@ -207,6 +207,9 @@ static const struct defaultserver def[] =
 	{"Hashmark",	0},
 	{0,			"irc.hashmark.net"},
 
+	{"ICQ-Chat", 0, 0, 0, LOGIN_SASL, 0, TRUE},
+	{0,			"irc.icq-chat.com/+6697"},
+
 	{"IdleMonkeys", 0},
 	{0,			"irc.idlemonkeys.net"},
 
