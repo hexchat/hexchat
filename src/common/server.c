8239fbd041abdfbc17ed605a8190a62f6825beb4
Sadie Powell <sadie@witchery.services>
2021-08-23 23:30:32 +0100

    Be a bit less insulting about servers with longer line lengths.

--- src/common/server.c
+++ src/common/server.c
@@ -362,7 +362,7 @@ server_read (GIOChannel *source, GIOCondition condition, server *serv)
 				serv->linebuf[serv->pos] = lbuf[i];
 				if (serv->pos >= (sizeof (serv->linebuf) - 1))
 					fprintf (stderr,
-								"*** HEXCHAT WARNING: Buffer overflow - shit server!\n");
+								"*** HEXCHAT WARNING: Buffer overflow - non-compliant server!\n");
 				else
 					serv->pos++;
 			}
